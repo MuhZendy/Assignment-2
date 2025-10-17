791d7fa9b5cbb83ff447800652c1cc871bec2fb7
Muhammad Zendy Zainuddin <zendyzainuddin425@gmail.com>
2025-10-17 19:41:04 -0400

    fixed getNumberOfSubBreeds

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -29,7 +29,7 @@ public class Main {
         try {
             breeds = breedFetcher.getSubBreeds(breed);
         } catch (BreedFetcher.BreedNotFoundException e) {
-            return -1;
+            return 0;
         }
         return breeds.size();
     }
